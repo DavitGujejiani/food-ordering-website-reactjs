952533a0171a124835d187fb3cf781c737c7d66d
DavitGujejiani <gujejiani.daviti@gmail.com>
2021-07-21 08:25:51 +0400

    144. Added forwardRef to input component - can add item to cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
+import { useRef, useState } from "react";
 
 function MealItemForm(props) {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (
+            enteredAmount.trim().length === 0 ||
+            enteredAmountNumber < 1 ||
+            enteredAmountNumber > 5
+        ) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.addToCart(enteredAmountNumber)
+    }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input
+                ref={amountInputRef}
                 label="Amount"
                 input={{
                     id: props.id,
@@ -16,6 +39,7 @@ function MealItemForm(props) {
                 }}
             />
             <button>Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     );
 }
