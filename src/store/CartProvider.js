f30376cc0676124a2963c0b0d669baa2cd6cbbbd
DavitGujejiani <gujejiani.daviti@gmail.com>
2021-07-21 10:15:27 +0400

    146. Existing cart product amount increments when adding

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -13,9 +13,27 @@ const defaultCartState = {
 
 function cartReducer(state, action) {
     if (action.type === ACTIONS.ADD_ITEM) {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount =
             state.totalAmount + action.item.price * action.item.amount;
+
+        // if item is already in cart - {if true - assign item to 'existingCartItem' constant}
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount,
