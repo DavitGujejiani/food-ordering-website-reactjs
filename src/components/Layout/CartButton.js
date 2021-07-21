2e5d2129c04689a91a178bfda5cfac4b5a1147ac
DavitGujejiani <gujejiani.daviti@gmail.com>
2021-07-21 11:05:34 +0400

    148. Added bump effect to cart button on cart items change

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,15 +1,35 @@
 import classes from "./CartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 function CartButton(props) {
-    const cartCtx = useContext(CartContext);
-    const cartItemsCount = cartCtx.items.reduce((currentNumber, item) => {
+    const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+    const { items } = useContext(CartContext);
+    const cartItemsCount = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0);
+
+    const btnClasses = `${classes.button} ${
+        buttonIsHighlighted ? classes.bump : ""
+    }`;
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setButtonIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setButtonIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
