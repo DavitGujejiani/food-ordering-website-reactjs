c4a01889ce2c61e47a30f776c2076eb72c953579
DavitGujejiani <gujejiani.daviti@gmail.com>
2021-07-21 08:43:09 +0400

    145. Outputting cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,31 @@
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 function Cart(props) {
-    const cartItems = [
-        { id: "c1", name: "Sushi", amount: 2, price: 12.99 },
-    ].map((item) => <li>{item.name}</li>);
+    const cartCtx = useContext(CartContext);
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    function cartItemRemoveHandler(id) {
+
+    }
+    function cartItemAddHandler(item) {
+
+    }
+
+    const cartItems = cartCtx.items.map((item) => (
+        <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            removeItem={cartItemRemoveHandler.bind(null, item.id)}
+            addItem={cartItemAddHandler.bind(null, item)}
+        />
+    ));
 
     return (
         <Modal onClick={props.hideCart}>
@@ -12,11 +33,18 @@ function Cart(props) {
                 <ul className={classes["cart-items"]}>{cartItems}</ul>
                 <div className={classes.total}>
                     <span>Total Amount</span>
-                    <span>35.62</span>
+                    <span>{totalAmount}</span>
                 </div>
                 <div className={classes.actions}>
-                    <button className={classes['button--alt']} onClick={props.hideCart}>Close</button>
-                    <button className={classes.button}>Order</button>
+                    <button
+                        className={classes["button--alt"]}
+                        onClick={props.hideCart}
+                    >
+                        Close
+                    </button>
+                    {hasItems && (
+                        <button className={classes.button}>Order</button>
+                    )}
                 </div>
             </div>
         </Modal>
